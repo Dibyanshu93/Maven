9582631e06a4b0c398a3ac880e5c2bfcd913b4a3
TYSS <TYSS@TYSS-TR-0017>
2018-06-18 16:43:35 +0530

    dhtd

---
+++ Maven/src/org/tyss/com/XYZ.java
@@ -0,0 +1,14 @@
+package org.tyss.com;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class XYZ {
+	
+	@Test
+	public void demoTest()
+	{
+		Reporter.log("helloooooooooo");
+	}
+
+}
